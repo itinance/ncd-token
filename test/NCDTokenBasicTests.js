c7dd1e8a2f8ac33aaacb86cb14d35f4577d7f829
Hagen Hübel <hhuebel@itinance.com>
2019-06-12 10:13:02 +0200

    Team Token Vesting prepared

--- test/NCDTokenBasicTests.js
+++ test/NCDTokenBasicTests.js
@@ -5,7 +5,6 @@ const { expect } = require('chai');
 
 const NCDToken = artifacts.require('NCDToken');
 
-
 contract("NCDToken", async ([_, owner, minter, pauser1, pauser2, ...otherAccounts]) => {
     let token;
 
--- test/NCDTokenSaleTests.js
+++ test/NCDTokenSaleTests.js
@@ -5,9 +5,8 @@ const { expect } = require('chai');
 const { ZERO_ADDRESS } = constants;
 
 const NCDToken = artifacts.require('NCDToken');
-const NCDTokenSale = artifacts.require('NCDTokenSaleImpl');
 
-const NCDTokenSale_ = artifacts.require('NCDTokenSale');
+const NCDTokenSale = artifacts.require('NCDTokenSale');
 
 
 contract("CrowdSale tests", async ([_, owner, pauser1, pauser2,  ...otherAccounts]) => {
@@ -29,48 +28,58 @@ contract("CrowdSale tests", async ([_, owner, pauser1, pauser2,  ...otherAccount
         closingTime = openingTime.add(time.duration.years(1));
         afterClosingTime = closingTime.add(time.duration.seconds(1));
 
-        console.log(1, openingTime.toString())
-        console.log(2, closingTime.toString());
-        console.log(3, afterClosingTime.toString());
-        console.log(4, token.address);
-
-
-        //tokenSale = await NCDTokenSale.new(openingTime, closingTime, token.address, {from: owner});
-        tokenSale = await NCDTokenSale_.new({from: owner});
+        tokenSale = await NCDTokenSale.new({from: owner});
         await tokenSale.initialize(openingTime, closingTime, token.address);
 
-
-        console.log(5)
 
         await token.addMinter(tokenSale.address, {from: owner});
         await token.renounceMinter({ from: owner });
 
-        console.log(7)
-
     });
 
 
-//    describe("Initiating token sale", function() {
+    it('crowdsale should be minter', async function () {
+      (await token.isMinter(tokenSale.address)).should.equal(true);
+    });
 
-        it('crowdsale should be minter', async function () {
-          (await token.isMinter(tokenSale.address)).should.equal(true);
-        });
+    it('owner should not be minter anymore', async function () {
+      (await token.isMinter(owner)).should.equal(false);
+    });
 
-        it('owner should not be minter anymore', async function () {
-          (await token.isMinter(owner)).should.equal(false);
-        });
+    it('Timelock can be added', async function() {
+      const
+        vestingStart1 = afterClosingTime,
+        vestingRelease1 = afterClosingTime.add(time.duration.days(10)),
 
-        it('Timelock can be added', async function() {
-          await tokenSale.addTimelock(afterClosingTime);
-        })
+        vestingStart2 = vestingRelease1.add(time.duration.seconds(1)),
+        vestingRelease2 = vestingStart2.add(time.duration.days(10))
+        ;
 
-        it('token can be minted in the crowdsale', async function() {
-            await tokenSale.mintTokens(buyer, 1000);
+      let {logs} = await tokenSale.addVestingLock(vestingStart1, vestingRelease1);
+      expectEvent.inLogs(logs, 'VestingLockAdded', {
+        vestingPeriodStart: vestingStart1,
+        releaseTime: vestingRelease1,
+      });
 
-            const balance = await token.balanceOf(buyer);
-            expect(balance).to.be.bignumber.equal('1000');
-        })
-//    })
+      const tx = await tokenSale.addVestingLock(vestingStart2, vestingRelease2);
+      logs = tx.logs;
+      expectEvent.inLogs(logs, 'VestingLockAdded', {
+        vestingPeriodStart: vestingStart2,
+        releaseTime: vestingRelease2,
+      });
+    })
 
+    it('token can be minted in the crowdsale', async function() {
 
+        // minting 1000 tokens
+        await tokenSale.mintTokens(buyer, 1000);
+
+        const balance = await token.balanceOf(buyer);
+        expect(balance).to.be.bignumber.equal('1000');
+
+        // Team Tokens was counted the right way
+        expect(await tokenSale.getTeamTokensTotal()).to.be.bignumber.equal('1000');
+        expect(await tokenSale.getTeamTokensUnreleased()).to.be.bignumber.equal('1000');
+        expect(await tokenSale.getTeamTokensReleased()).to.be.bignumber.equal('0');
+    })
 });
