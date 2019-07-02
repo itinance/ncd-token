a6736a7e3c16f5ac4a5756d4ef5370a54131d53c
cgewecke <christophergewecke@gmail.com>
2019-07-02 10:36:46 -0700

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -51,19 +51,31 @@ module.exports = {
       network_id: 1,
       gas: 8000000,
       gasPrice: 10000000000
-    }
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    },
   },
   mocha: {
-    reporter: 'eth-gas-reporter',
+    reporter: (!process.env.SOLIDITY_COVERAGE) ? 'eth-gas-reporter' : 'spec',
     reporterOptions : {
       currency: 'EUR',
       gasPrice: 21
     }
   },
-  solc: {
-    optimizer: {
-        enabled: true,
-        runs: 200,
+  compilers: {
+    solc: {
+      version: "0.5.7",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+      }
     }
   }
 };
